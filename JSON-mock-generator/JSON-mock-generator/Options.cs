844ec2ee6be541fbc4aca16fa8bedf0b9aff96f3
Hai Nguyen <hai2502@gmail.com>
2021-05-29 21:38:25 +1000

    Initial commit

---
+++ JSON-mock-generator/JSON-mock-generator/Options.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine.Text;
+using CommandLine;
+
+namespace JSON_mock_generator
+{
+    class Options
+    {
+        [Option("output", Required = false, HelpText = "Output JSON mock data filename.")]
+        public string outputFileName { get; set; }
+
+        [Option("number", Required = true, HelpText = "Number of record to generate.")]
+        public string number { get; set; }
+
+        [Option("invalid", Required = false, HelpText = "To generate invlid data. Y/N")]
+        public string isInvalid { get; set; }
+
+        [Usage(ApplicationAlias = "json-mock-generator")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                return new List<Example>() {
+                    new Example("Generate mnock test data based on a provided template", new Options { outputFileName = "testdata.json", number = "1000", isInvalid = "N" })
+                };
+            }
+        }
+    }
+}
---
+++ JSON-mock-generator/JSON-mock-generator/Program.cs
@@ -0,0 +1,125 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JSON_mock_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(n =>
+                {
+                    try
+                    {
+                        int numberOfRecord = int.Parse(n.number), rndLength = 0;                    
+                        DataTable dt = new DataTable();
+                        string outputFilePath = "";
+                        if (numberOfRecord > 0)
+                        {
+                            dt.Columns.Add("GUID", typeof(string));
+                            dt.Columns.Add("Name", typeof(string));
+                            dt.Columns.Add("Date", typeof(DateTime));
+                            dt.Columns.Add("State", typeof(string));
+                            for (int i = 1; i <= numberOfRecord; i++)
+                            {
+                                // generate GUID
+                                Guid guid = Guid.NewGuid();
+                                // generate random unique name                  
+                                Random rnd = new Random();
+                                if (!String.IsNullOrEmpty(n.isInvalid) && n.isInvalid == "Y")
+                                {
+                                    rndLength = rnd.Next(257, 512);
+                                }
+                                else
+                                {
+                                    rndLength = rnd.Next(1, 256);                                    
+                                }
+                                string uniqueName = UniqueASCIIName(dt, rndLength);
+                                // generate Date from 1970 to 2010
+                                DateTime from = new DateTime(1970, 01, 01);
+                                DateTime to = new DateTime(2010, 12, 31);
+                                DateTime date = GetRandomDate(from, to);
+                                // generate State
+                                string state = RandomEnumValue<data.StateEnum>().ToString();
+                                dt.Rows.Add(guid, uniqueName, date, state);
+                            }
+                        }
+                        if (String.IsNullOrEmpty(n.outputFileName))
+                        {
+                            outputFilePath = "testdata.json";                            
+                        }
+                        else {
+                            outputFilePath = n.outputFileName;
+                        }
+                        string JSONString = JsonConvert.SerializeObject(dt, Formatting.Indented);
+                        using (StreamWriter outputFile = new StreamWriter(outputFilePath))
+                        {
+                            outputFile.WriteLine(JSONString);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                })
+                .WithNotParsed<Options>(e =>
+                {
+                    Console.WriteLine("Invalid argument(s) provided. Please double check your input and try again.");
+                });
+        }
+
+        public static String UniqueASCIIName(DataTable dt, int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // currently only test ASCII
+
+            var random = new Random();
+            var randomString = new string(Enumerable.Repeat(chars, length)
+                                                    .Select(s => s[random.Next(s.Length)]).ToArray());
+            bool contains = true; // set to true in order to get into the loop. The value will be reset if there is no match
+            while (contains)
+            {
+                // check if name is already generated
+                contains = dt.AsEnumerable().Any(row => randomString == row.Field<String>("Name")); 
+                if (!contains)
+                    break; // name is unique
+                randomString = UniqueASCIIName(dt, length);
+            }
+            return randomString;
+        }
+
+        public static DateTime GetRandomDate(DateTime from, DateTime to)
+        {
+            var range = to - from;
+            Random rnd = new Random();
+            var randTimeSpan = new TimeSpan((long)(rnd.NextDouble() * range.Ticks));
+            return from + randTimeSpan;
+        }
+
+        public static T RandomEnumValue<T>()
+        {
+            Random rnd = new Random();
+            var values = Enum.GetValues(typeof(T));
+            return (T)values.GetValue(rnd.Next(values.Length));
+        }
+    }
+
+    public class data
+    {
+        public enum StateEnum
+        {
+            New, Acitve, Complete
+        }
+        public string GUID { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public StateEnum State { get; set; }
+    }
+}
